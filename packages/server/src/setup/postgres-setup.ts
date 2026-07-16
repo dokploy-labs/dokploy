8bf87b6e899f0a46e4765f801803243a61e4d467
adramarilla <adriana.amarilla@unahur.edu.ar>
2026-07-16 04:57:34 -0300

    feat(dev): make PostgreSQL development port configurable

--- packages/server/src/setup/postgres-setup.ts
+++ packages/server/src/setup/postgres-setup.ts
@@ -37,7 +37,7 @@ export const initializePostgres = async () => {
 				Ports: [
 					{
 						TargetPort: 5432,
-						PublishedPort: 5432,
+						PublishedPort: Number(process.env.POSTGRES_PUBLISHED_PORT ?? "5432"),
 						Protocol: "tcp",
 						PublishMode: "host",
 					},
